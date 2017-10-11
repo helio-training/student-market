5e09519e4c40eb5bdfbcb4f721aed4d4eb0f51ef
DanielPeckham <d.moroni.peckham@gmail.com>
2017-10-11 13:38:29 -0600

    Basic structure

---
+++ src/Routes/Products/dragon-eggs.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default (props) => {
+  const dragonEggs = [
+    {
+      src: '',
+      cardTitle: 'Black Dragon',
+      cardText: 'Black Dragons will grow up to be vile, evil tempered, and obsessed with death. The prefer cold, humid climates. They are perfect for guarding your treasure trove.',
+      Price: '$50,000'
+    },
+    {
+      title: 'Red Dragon',
+      description: 'Red dragons are greedy and covetous, and obsessed with increasing their treasure hoards. They prefer hot habitats.',
+      price: '$52,000'
+    },
+    {
+      title: 'Blue Dragon',
+      description: 'Blue dragons are pensive, lawful, and vain. They live in hot, dry areas, such as sandy deserts or arid steppes. These are the only possible candidates for pets.',
+      price: '$45,000',
+    },
+    {
+      title: 'Green Dragon',
+      description: 'Green dragons are cruel, belligerent creatures and masters of intrigue, politics, and backbiting. They prefer forests—the older and bigger the trees, the better. Instead of being overtly aggressive, they prefer to concoct elaborate schemes to gain power or wealth with as little effort as possible',
+      price: ''
+    }
+  ]
+}
