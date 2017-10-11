52de266e3feecbb7b849337ebc6ad9f7bec61619
kelsrwalker <kelsrwalker@yahoo.com>
2017-10-11 15:10:20 -0600

    nav bar and wine.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Navigation from "./Routes/Navigation/component";
 
-
+import WineProducts from './Routes/Products/wine'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Navigation/>
+        <WineProducts/>
       </div>
     );
   }
