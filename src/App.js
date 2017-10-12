6794925ac5797e1b993018fd11afa0b19ac667dc
Olga Allen <olyaallen@gmail.com>
2017-10-12 10:56:49 -0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import Header from './Routes/Header/component'
-import Chickens from './Routes/Products/chickens'
+// import Chickens from './Routes/Products/chickens'
 import Watches from './Routes/Products/watches'
+import Shoes from './Routes/Products/shoes'
 import Search from './Routes/Navigation/search-bar/component'
 import Account from './Routes/Header/component'
-
-
+import imgUrl from './Assets/img.png'
+import './App.css'
 class App extends Component {
   render() {
     return (
       <div>
+
+        <img src={imgUrl} />
         <Header/>
-        <Chickens/>
-        <Watches/>
-        <Search/>
-        <Account/>
+        <Shoes/>
       </div>
     );
   }
