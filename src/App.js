52d7b1faa23de9ddac4a9d17ce1115089c84b0b7
DanielPeckham <d.moroni.peckham@gmail.com>
2017-10-12 10:24:01 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
-import Header from './Routes/Header/component'
-// import Chickens from './Routes/Products/chickens'
-import Watches from './Routes/Products/watches'
-import Search from './Routes/Navigation/search-bar/component'
-import Account from './Routes/Header/component'
-import Navigation from './Routes/Navigation/component'
-
+import React, { Component } from 'react'
+import Routes from './Routes/component'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Navigation/>
-        {/*<Chickens/>*/}
-        <Watches/>
+        <Routes/>
       </div>
     );
   }
