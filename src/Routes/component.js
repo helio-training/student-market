de45953d12f945f4b0d665680df05664f848fb53
Olga Allen <olyaallen@gmail.com>
2017-10-12 11:01:16 -0600

    Add shoes route

--- src/Routes/component.js
+++ src/Routes/component.js
@@ -3,6 +3,7 @@ import Header from './Header/component'
 import Navigation from './Navigation/component'
 import Chickens from './Products/chickens'
 import DragonEggs from './Products/dragon-eggs'
+import Shoes from './Products/shoes'
 import Watches from './Products/watches'
 import Wine from './Products/wine'
 import { Switch, Route } from 'react-router-dom'
@@ -17,6 +18,7 @@ export default class Routes extends Component {
         <Switch>
           <Route path='/chickens' component={Chickens}/>
           <Route path='/dragon-eggs' component={DragonEggs}/>
+          <Route path='/shoes' component={Shoes}/>
           <Route path='/watches' component={Watches}/>
           <Route path='/wine' component={Wine}/>
         </Switch>
