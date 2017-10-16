7a31d49de715110a789779450fa856784e0934de
Keller Beckstead <kellerjbeckstead@gmail.com>
2017-10-16 13:45:22 -0600

    restructure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
