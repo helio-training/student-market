ea6be584f157c0a901636cae76e0257c984396c4
kelsrwalker <kelsrwalker@yahoo.com>
2017-10-17 11:02:33 -0600

    Updat

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Navigation from "./src/navigation/component";
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <navigation/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import store from './store'
 
